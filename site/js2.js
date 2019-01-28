30b9d2623562727421a9206500bf354d8268af8b
Tiran132 <32066820+Tiran132@users.noreply.github.com>
2019-01-28 22:28:08 +0700

    Add files via upload

--- site/js2.js
+++ site/js2.js
@@ -1,13 +1,18 @@
 let page = document.getElementById('bag');
+let buylist = document.getElementById('buylist');
 
 function load(){
 	page.innerHTML = '';
+	buylist.value = '';
 for (var i = 0; i < localStorage.length; i++) {
 	page.innerHTML += '<div>' + "<img class=\"tov\" src=\"img/" +
 	 localStorage.key(i) +"\">" + '<span>Артикль: '
 	  + localStorage.getItem(localStorage.key(i)) + '</span>' + 
 	  '<button onclick=\"rmv(\''+localStorage.key(i)+'\')\">del</button>' + '</div>';
+
+	buylist.value += localStorage.getItem(localStorage.key(i)) + ' ';
 }	
+
 }
 function clr(){
 	localStorage.clear();
